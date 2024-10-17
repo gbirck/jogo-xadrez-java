0bd00885b81ee482652d0f3568e649e30978365e
Gabriel Birck <birckzin@gmail.com>
2024-10-17 14:59:07 -0300

    Projeto criado

---
+++ src/Peca.java
@@ -0,0 +1,16 @@
+public class Peca {
+
+    protected Posicao posicao;
+
+    public Peca(Posicao posicao) {
+        this.posicao = posicao;
+    }
+
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Posicao posicao) {
+        this.posicao = posicao;
+    }
+}
